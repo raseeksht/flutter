81cc5bb2ddf258b1937525044329e615e4b7628e
raseeksht <raseeksht1@gmail.com>
2023-09-27 11:06:06 +0545

    home design superheross

--- lib/home.dart
+++ lib/home.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:untitled/model/sample_model.dart';
 import 'package:untitled/repository/sample_repository.dart';
 
-
 class Home extends StatefulWidget {
   Home({super.key});
 
   List<Superheros>? superheros;
-
-
 
   @override
   State<Home> createState() => _HomeState();
@@ -16,34 +13,56 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Superheros>? superheros;
+
   @override
-  void initState(){
+  void initState() {
     getApiData();
     super.initState();
-
-
   }
 
-  void getApiData()async {
+  void getApiData() async {
     final sampleRepo = SampleRepository();
     final superHeroslist = await sampleRepo.sampleApiCall();
     setState(() {
       superheros = superHeroslist;
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: superheros!=null? superheros!.length:0,
-        itemBuilder: (context,index){
-
-      return ListTile(
-        title: Text(superheros!=null ? superheros![index].name! : "Name") ,
-        subtitle: Text(superheros!=null ? superheros![index].power! : "Power") ,
-
-      );
-    });
+        itemCount: superheros != null ? superheros!.length : 0,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              ListTile(
+                title: Text(
+                  superheros != null ? superheros![index].name! : "Name",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                subtitle: Text(
+                  superheros != null ? superheros![index].power! : "Power",
+                  style: TextStyle(
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                  ),
+                ),
+                leading: ClipOval(
+                    child: Image.network(
+                  superheros != null
+                      ? superheros![index].url!
+                      : "https://upload.wikimedia.org/wikipedia/en/2/23/Deadpool_%282016_poster%29.png",
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
+                )),
+              ),
+              Divider(height: 20,color: Colors.black,thickness: 2,)
+            ],
+          );
+        });
   }
 }
