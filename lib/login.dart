798e28f0ee0f26dff86c64720d10339f3163183d
raseeksht <raseeksht1@gmail.com>
2023-10-04 11:15:02 +0545

    login + picture

--- lib/login.dart
+++ lib/login.dart
@@ -22,11 +22,27 @@ class Login extends StatelessWidget{
                 children: [
                   FractionallySizedBox(
                     widthFactor: 0.5,
-                    child: Text("hh left"),
+                    child: Container(
+                      height: constraints.maxHeight,
+                      width: constraints.maxWidth,
+                      decoration: BoxDecoration(color: Colors.blueAccent.withOpacity(0.3),borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomLeft: Radius.circular(20))),
+                      child: Center(
+                        child: Image.network("https://i.pinimg.com/originals/23/fc/e5/23fce59456f04047a434a5719cd914d9.jpg",height: constraints.maxHeight/2,),
+                      ),
+                    ),
                   ),
                   FractionallySizedBox(
                     widthFactor: 0.5,
-                    child: Text("right"),
+                    child: Container(
+                      // padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.only(top: 70,left: 20,right: 20),
+                      height: constraints.maxHeight / 1.3,
+                      width: constraints.maxWidth,
+                      decoration: BoxDecoration(color: Colors.greenAccent.withOpacity(0.3)),
+                      child: Center(
+                        // child: Image.network("src"),
+                      ),
+                    ),
                   )
                 ],
               );
